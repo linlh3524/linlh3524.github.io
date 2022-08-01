fbfa92bf0d83367fa13e40d804ff80d10e29b49e
lihong <2830174715@qq.com>
2022-08-01 16:57:12 +0800

    Add files via upload

---
+++ scripts/sentences.js
@@ -0,0 +1,39 @@
+const data = [{
+        "content": "不要哭泣，不要放大愤怒。一切都会渐渐清晰。",
+        "date": "2022-08-01",
+        "image": "https://images.pexels.com/photos/247431/pexels-photo-247431.jpeg?auto=compress&cs=tinysrgb&w=400",
+        "bigImage": "https://images.pexels.com/photos/247431/pexels-photo-247431.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        "content": "独处是一个检验，用它可以检测处一个人的灵魂的深度，测出一个人的真实感觉，他是否厌烦自己。一个连和自己都相处不好的人，很难有高质量的社会交往。",
+        "date": "2022-07-31",
+        "image": "https://images.pexels.com/photos/207247/pexels-photo-207247.jpeg?auto=compress&cs=tinysrgb&w=400",
+        "bigImage": "https://images.pexels.com/photos/207247/pexels-photo-207247.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        "content": "事物本身没有变化，唯一变化的，是我们的思维。",
+        "date": "2022-07-30",
+        "image": "https://images.pexels.com/photos/1128797/pexels-photo-1128797.jpeg?auto=compress&cs=tinysrgb&w=400",
+        "bigImage": "https://images.pexels.com/photos/1128797/pexels-photo-1128797.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        "content": `
+        <p>天上银河久不见，</p>
+        <p>人间沧桑已阅遍。</p>
+        <p>当初百般挣笼出，</p>
+        <p>何故此番甚惦念？</p>
+        
+        `,
+        "date": "2022-07-29",
+        "image": "https://images.pexels.com/photos/662994/pexels-photo-662994.jpeg?auto=compress&cs=tinysrgb&w=400",
+        "bigImage": "https://images.pexels.com/photos/662994/pexels-photo-662994.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+    {
+        "content": "唯有从每天的生活体验中创造乐趣，才能真正提升生活品质",
+        "date": "2022-07-28",
+        "image": "https://images.pexels.com/photos/235615/pexels-photo-235615.jpeg?auto=compress&cs=tinysrgb&w=400",
+        "bigImage": "https://images.pexels.com/photos/235615/pexels-photo-235615.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    }
+]
+
+export default data
